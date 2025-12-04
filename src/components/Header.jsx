1888ae71851bdec12f0f4f56e6f09bc6378b05ab
Aristides Rodríguez <12479256+arisrodriguez@users.noreply.github.com>
2025-12-04 00:25:38 -0500

    feat: crear estructura del proyecto y añadir Header y cards.js con arreglo de datos para tarjetas

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+export const Header = () => {
+    return (
+        <div className='flex flex-col items-center justify-center h-screen'>
+            <div className="w-[60%] space-y-4 text-center">
+                <h1 className="font-bold text-2xl text-blue-700">Bienvenido/a al proyecto de práctica</h1>
+                <p className="font-semibold text-xl text-blue-600">React + Tailwind CSS</p>
+                <p className="text-lg text-gray-600">Usaremos <b>Git</b>: Gestionar los cambios en el proyecto, control de versiones, trabajo por ramas y <b>GitHub</b> para alojar el repositorio facilitando la colaboración y gestión del proyecto de forma remota.</p>
+                <p className="font-bold">Empezamos el proyecto</p>
+            </div>
+        </div>
+    )
+}
