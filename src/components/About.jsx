5b98a7da48ef2e281cce243a0aaa247285885adb
Aristides Rodríguez <12479256+arisrodriguez@users.noreply.github.com>
2025-12-08 17:11:34 -0500

    style: ajustar estilos y mejorar el diseño de la sección About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,10 @@
 
 export const About = () => {
     return (
-        <div id="about" className="w-[1000px] m-auto space-y-2 my-8">
+        <div id="about" className="w-full max-w-[1000px] flex-col justify-center items-center
+         m-auto space-y-2 my-8 px-4">
             <h1 className="font-bold text-3xl">Cardify</h1>
-            <h3 className="">Tarjetas digitales listas para compartir y descargar.</h3>
+            <h3 className="italic">Tarjetas digitales listas para compartir y descargar.</h3>
             <div className="mt-4 space-y-3">
                 <p>Cardify es una colección moderna de tarjetas digitales de cumpleaños diseñadas para ahorrar tiempo y ampliar tu creatividad. Explora diseños elegantes, variados y listos para descargar en un solo clic.</p>
                 <p>Nuestro objetivo es brindarte tarjetas bonitas, rápidas de personalizar y fáciles de usar.</p>
