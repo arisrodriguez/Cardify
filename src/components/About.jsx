5cfc2ac726d9447191cbfd0282d8d8f5ebb4f30b
Aristides Rodríguez <12479256+arisrodriguez@users.noreply.github.com>
2025-12-08 16:41:39 -0500

    feat(about): agregar estructura inicial del componente About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,13 @@
 
 export const About = () => {
     return (
-        <div id="about">About</div>
+        <div id="about" className="w-[1000px] m-auto space-y-2 my-8">
+            <h1 className="font-bold text-3xl">Cardify</h1>
+            <h3 className="">Tarjetas digitales listas para compartir y descargar.</h3>
+            <div className="mt-4 space-y-3">
+                <p>Cardify es una colección moderna de tarjetas digitales de cumpleaños diseñadas para ahorrar tiempo y ampliar tu creatividad. Explora diseños elegantes, variados y listos para descargar en un solo clic.</p>
+                <p>Nuestro objetivo es brindarte tarjetas bonitas, rápidas de personalizar y fáciles de usar.</p>
+            </div>
+        </div>
     )
 }
